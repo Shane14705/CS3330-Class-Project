02641d48a1d6d3a0c62c52022b0df8b3c815aae2
Shane McKelvey <psmnhb@umsystem.edu>
2023-04-16 23:29:30 -0500

    Updated newspaper class to inherit and work with LibraryItem base class. Also added JavaDoc comments so we get hints on how to use the class in the editor

--- src/main/java/LibrarySystem/LibraryCollection/LibraryNewspaper.java
+++ src/main/java/LibrarySystem/LibraryCollection/LibraryNewspaper.java
@@ -2,17 +2,23 @@ package LibrarySystem.LibraryCollection;
 
 import java.util.Date;
 
-public class LibraryNewspaper {
+public class LibraryNewspaper extends LibraryItem {
     private int ISSN[];
     private Date Pub_Date;
     private String Publisher;
-    private char[] ID_num = new char[6];
 
-    //Get and set for ISSN
-    //Set for ISSN references its actual method, so don't worry about the strange calculations.
+    /**
+     * Gets the 8 digit ISSN number for this newspaper edition.
+     * @return Int array of 8 digits representing the valid ISSN of this newspaper edition
+     */
     public int[] getISSN() {
         return ISSN;
     }
+
+    /**
+     * Function to validate and update the ISSN number for this newspaper edition.
+     * @param ISSN Int array of 8 digits representing the valid ISSN of this newspaper edition
+     */
     public void setISSN(int[] ISSN) {
         if(ISSN.length != 8)
         {
@@ -34,35 +40,55 @@ public class LibraryNewspaper {
         this.ISSN = ISSN;
     }
 
-    //Get and set for the publication date.
+    /**
+     * Gets the date on which this newspaper edition was published.
+     * @return Date object representing the date on which this newspaper edition was published
+     */
     public Date getPub_Date() {
         return Pub_Date;
     }
+
+    /**
+     * Updates the date on which this newspaper edition was published.
+     * @param pub_Date Date object representing the date on which this newspaper edition was published
+     */
     public void setPub_Date(Date pub_Date) {
         Pub_Date = pub_Date;
     }
 
-    //Get and set for the publisher.
+    /**
+     * Gets the publisher of this newspaper edition.
+     * @return String representing the name of the publisher of this newspaper edition
+     */
     public String getPublisher() {
         return Publisher;
     }
+
+    /**
+     * Updates the publisher of this newspaper edition.
+     * @param publisher String representing the new name of the publisher of this newspaper edition
+     */
     public void setPublisher(String publisher) {
         Publisher = publisher;
     }
 
-    //Get and set for the ID number.
-    public char[] getID_num() {
-        return ID_num;
-    }
-    public void setID_num(char[] ID_num){
-        this.ID_num = ID_num;
-    }
 
-    //Primary constructor for all instance variables.
-    public LibraryNewspaper(int[] ISSN, Date Pub_Date, String Publisher, char[] ID_num){
+
+
+    //TODO: Finish filling out the documentation for the parameters here
+    /**
+     * Constructs a Newspaper item for the collection. Each edition of a Newspaper in the library should be represented by one instance of this object
+     * @param ISSN
+     * @param Pub_Date
+     * @param Publisher
+     * @param ID_num Character array containing 6 chars to represent the 6-digit alphanumeric unique ID for this newspaper edition in the collection
+     * @param price Float representing the price of one copy of this newspaper edition (in case it needs to be replaced or new copies need to be bought)
+     * @param numberAvailable Int representing the number of copies of this newspaper edition that will currently be available to borrow
+     */
+    public LibraryNewspaper(int[] ISSN, Date Pub_Date, String Publisher, char[] ID_num, float price, int numberAvailable){
+        super(ID_num, LibrarySection.NEWSPAPERS, price, numberAvailable);
         setISSN(ISSN);
         setPub_Date(Pub_Date);
         setPublisher(Publisher);
-        setID_num(ID_num);
     }
 }
